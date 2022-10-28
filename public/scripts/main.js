455341c11ef3bb7b1703d1dc87f0c9075c825312
Canon <92479910+rhit-marandcp@users.noreply.github.com>
2022-10-28 14:45:09 -0400

    Puzzle completion updates from Firebase

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -77,6 +77,24 @@ app.UserDatabaseManager = class {
     }
 }
 
+app.HomePageController = class {
+    constructor() { }
+
+    setPuzzleStatus() {
+        app.database.data.then(doc => {
+            let puzzleStatus = doc.data().puzzlesCompleted;
+            for (let puzzle in puzzleStatus) {
+                let puzzleElement = document.getElementById(puzzle);
+                if (puzzleStatus[puzzle]) {
+                    puzzleElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -32 576 576" width="1em" height="1em" fill="currentColor" class="complete"><path d="M352 192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H288V144C288 64.47 352.5 0 432 0C511.5 0 576 64.47 576 144V192C576 209.7 561.7 224 544 224C526.3 224 512 209.7 512 192V144C512 99.82 476.2 64 432 64C387.8 64 352 99.82 352 144V192z"></path></svg>' + puzzleElement.innerHTML;
+                } else {
+                    puzzleElement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="-32 0 512 512" width="1em" height="1em" fill="currentColor"> <path d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z"></path></svg>' + puzzleElement.innerHTML;
+                }
+            }
+        });
+    }
+}
+
 app.checkForRedirects = () => {
     const onLoginPage = window.location.pathname == '/' || window.location.pathname == '/index.html';
     if (app.auth.isSignedIn) {
@@ -120,11 +138,16 @@ app.pageManager = () => {
         document.querySelector('#authHeader').onclick = () => app.auth.signOut();
     if (window.location.pathname == '/welcome.html')
         document.querySelector('#startBtn').onclick = () => app.database.createUserData();
+    if (window.location.pathname == '/puzzles-home.html') {
+        app.pageController = new app.HomePageController();
+        app.pageController.setPuzzleStatus();
+    }
     app.startTimer();
 }
 
 app.auth = null;
 app.database = null;
+app.pageController = null;
 
 app.main = () => {
     app.auth = new app.AuthManager();
