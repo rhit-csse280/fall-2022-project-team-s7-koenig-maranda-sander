d126abce25f18c1b596d4b1e23a17f688db0f1cf
rhit-koenigjl <89945304+rhit-koenigjl@users.noreply.github.com>
2022-11-12 17:55:40 -0500

    completed newspaper

--- public/scripts/newspaper/newspaper.js
+++ public/scripts/newspaper/newspaper.js
@@ -1,5 +1,5 @@
 const newspaper = {} || newspaper;
-newspaper.password = String(Math.floor(Math.random() * 26 + 1));
+newspaper.password = String(Math.floor(Math.random() * 26 + 1)) + '|' + String(Math.floor(Math.random() * 10 + 9));
 newspaper.alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 newspaper.Newspaper = class {
@@ -11,6 +11,10 @@ newspaper.Newspaper = class {
 
         this.handleArticle();
         this.handleAdd();
+
+        this.handleColumn();
+
+        console.log('newspaper.password :>> ', newspaper.password);
     }
 
     handleArticle() {
@@ -33,6 +37,46 @@ newspaper.Newspaper = class {
         article.innerHTML = newTxt;
     }
 
+    handleColumn() {
+        const columnText = document.querySelector('#column > p');
+        let workingText = columnText.innerText.split('[the');
+        let initialThes = workingText.length - 1;
+        let maxThes = initialThes * 2;
+
+        let myNumThes = newspaper.password.split('|')[1];
+
+        console.log('initialThes :>> ', initialThes);
+        console.log('maxThes :>> ', maxThes);
+        console.log('myNumThes :>> ', myNumThes);
+
+        let openSlots = [];
+        for (let i = 0;i < initialThes;i ++) {
+            openSlots.push(i);
+        }
+
+        let chosenSlots = [];
+        for (let i = 0;i < myNumThes - initialThes;i ++) {
+            let poppedItem = openSlots.pop(Math.floor(Math.random() * openSlots.length));
+            chosenSlots.push(poppedItem);
+        }
+
+        let newInnerText = "";
+        for (let i = 0;i < workingText.length;i ++) {
+            newInnerText += workingText[i];
+            if (i == workingText.length - 1) {
+                continue;
+            }
+            if (chosenSlots.includes(i)) {
+                newInnerText += 'the the';
+            } else {
+                newInnerText += 'the';
+            }
+        }
+        
+        console.log(workingText);
+        columnText.innerHTML = newInnerText;
+    }
+
     handleAdd() {
         document.querySelector('#aValue').innerHTML = document.querySelector('#aValue').innerHTML.replace('[INSERT]', this.examinedLetter);
     }
@@ -41,7 +85,6 @@ newspaper.Newspaper = class {
 
 
 function main() {
-    console.log(newspaper.password);
     new newspaper.Newspaper(newspaper.password);
 };
 main();
